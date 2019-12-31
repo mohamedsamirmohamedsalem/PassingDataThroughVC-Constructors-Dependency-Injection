79528fda1bb93a7fec9869916c5898955a0b3f3c
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2019-12-31 17:47:18 +0200

    Initial Commit

---
+++ PassingDataThroughVC-Constructors-Dependency-Injection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingDataThroughVC-Constructors-Dependency-Injection
+//
+//  Created by Mohamed Samir on 12/31/19.
+//  Copyright © 2019 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
